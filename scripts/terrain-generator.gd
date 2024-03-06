ee861eebeb6a794c6f08b803a1c81bba3d48fd7e
Ryan Cross <ryan.cross.home@gmail.com>
2024-03-06 12:27:47 -0600

    fixing parsing function off by one and missing constraint issues, initialize wave, 1D to 2D index conversion

--- scripts/terrain-generator.gd
+++ scripts/terrain-generator.gd
@@ -11,63 +11,82 @@ const LAYER_ZERO := 0
 
 @onready
 var inputMap = $InputTileMap
-var outputSizeX = 0;
-var outputSizeY = 0;
-## Constraints is set of tile atlas coordinates (Vector2i) to an array of constraint rules. Array[ConstraintTuple] (use dict)
-
-
-@onready
-var testTuple := ConstraintTuple.new(LEFT, RIGHT, DOWN)
-var testTuple2 := ConstraintTuple.new(LEFT, RIGHT, RIGHT)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(testTuple.equals(testTuple2))
-	parseInputTileMap(inputMap)
-
+	var tilesToConstraints = parseInputTileMap(inputMap)
+	var wave = initializeWave(tilesToConstraints, inputMap)
+	
+	print(wave)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-#TODO
 	
 func parseInputTileMap(map : TileMap):
 	var tilesToConstraints : Dictionary
-	var constraintTuples : Dictionary
 	var rect = map.get_used_rect()
+	var mapWidth = rect.size.x
+	var mapHeight = rect.size.y 
 	
-	for x in rect.size.x: # width
-		for y in rect.size.y: # height
+	for x in mapWidth: # width
+		for y in mapHeight: # height
+
 			var tileId = map.get_cell_atlas_coords(LAYER_ZERO, Vector2i(x,y))
 
 			# initialize constraint rules array for tile type if one does not exist yet
 			if !tilesToConstraints.has(tileId):
-				constraintTuples = {}
+				var constraintTuples = {}
 				tilesToConstraints[tileId] = constraintTuples
-			else:
-				constraintTuples = tilesToConstraints.get(tileId)	
 			
 			for i in DIRECTIONS.size():
-				var currentCell = Vector2i(x,y)
-				var targetCell = Vector2i(x + DIRECTIONS[i].x, y + DIRECTIONS[i].y)				
+				var targetCell = Vector2i(x + DIRECTIONS[i].x, y + DIRECTIONS[i].y)			
+				
 				# if checking past map bounds, skip constraint generation
-				if targetCell.x < 0 || targetCell.y < 0:
+				if targetCell.x < 0 or targetCell.y < 0:
 					continue
-				
+				if targetCell.x >= mapWidth or targetCell.y >= mapHeight:
+					continue
 				# Sets don't exist in Godot, so we'll use a dict instead
-				var targetTile = map.get_cell_atlas_coords(LAYER_ZERO, targetCell)	
-				var constraint = { tileId : true, targetTile: true, DIRECTIONS[i] : true}
-				if constraintTuples.size() == 0:
-					constraintTuples = constraint
+				var targetTile = map.get_cell_atlas_coords(LAYER_ZERO, targetCell)
+				var constraint = { "local" : tileId, "allowed": targetTile, "direction" : DIRECTIONS[i]} 
+				if tilesToConstraints.size() == 0:
+					tilesToConstraints[constraint] = constraint
 				else:
-					constraintTuples.merge(constraint)
-				
-				tilesToConstraints[tileId] = constraintTuples
-	return tilesToConstraints
+					tilesToConstraints[tileId].merge({constraint: true})
+
+	return tilesToConstraints #TODO test addition of tiles
 	
+	# The output array is known as a "wave" with the dimensions of a coefficient matrix. 
+	# The coefficient matrix is described in the algorithm as all possible states for an NxM region of pixels. 
+	# For the even simpler tiled model, a region is one tile on the map, so the possibility state is simply the number of tiles
+func calculateCoefficientMatrix(numTileTypesUsed: int):
+	var coefficient = numTileTypesUsed
+	print("Coefficient is:", coefficient)
+	return coefficient
 
-
-
-
+func initializeWave(tilesToConstraints: Dictionary, inputMap: TileMap):
+	var rect = inputMap.get_used_rect()
+	var mapWidth = rect.size.x
+	var mapHeight = rect.size.y 
 	
+	var wave = createMatrix(mapWidth, mapHeight)
+	var matrixCoefficient = calculateCoefficientMatrix(tilesToConstraints.size())
+	wave.fill(matrixCoefficient)
+
+	return wave
+	
+func createMatrix(width, height) -> Array[Variant]: 
+	var matrix = Array()
+	matrix.resize(width * height)
+	return matrix
+
+func idx2DtoIdx1D(x, y, width) -> int:
+	var index = (x * width) + y
+	return index
+
+func getRandomIdx(waveSize) -> int:
+	return randi() % (waveSize - 1)
+
+
